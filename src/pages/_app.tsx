6dc572806ba6c0782cec3033560d988c6bd80213
tsuka414 <keita04141537@icloud.com>
2024-03-08 19:20:40 +0900

    create CreateTodo component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { Inter } from "next/font/google";
+import { Toaster } from "react-hot-toast";
 
 import { api } from "~/utils/api";
 
@@ -18,9 +19,8 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <main className={`font-sans ${inter.variable}`}>
+      <Toaster />
         <Component {...pageProps} />
-      </main>
     </SessionProvider>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
-import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-import Link from "next/link";
-
-import { api } from "~/utils/api";
+import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { CreateTodo } from "~/components/CreateTodo";
+// import { Todos } from "~/components/Todos";
 
 function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
   const { data: sessionData, status } = useSession();
-
   return (
     <>
       <Head>
@@ -18,9 +16,11 @@ function Home() {
       <div className="min-h-screen bg-olive-one p-0 selection:bg-green-two md:py-24 md:px-8">
         <main className="mx-auto min-h-screen max-w-none rounded-none bg-cream-four px-5 pt-24 pb-10 outline-none md:max-w-[60rem] md:rounded-2xl md:px-8 md:outline md:outline-4 md:outline-offset-8 md:outline-cream-four">
           <h1 className="mb-6 text-center text-4xl font-bold text-gray-three">
-            ToDO List
+            ToDo List
           </h1>
           {status !== "loading" && sessionData && (
+            // status が "loading" でない、つまり認証情報の取得が完了している、
+            // かつ、認証されている場合に、下記が表示されます
             <>
               <div className="flex flex-col items-center">
                 <p className="text-l text-white mb-4 text-center">
@@ -33,11 +33,14 @@ function Home() {
                   Sign out
                 </button>
               </div>
-              <div>Todo componets coming soon...</div>
+              <div>
+                <CreateTodo />
+                <Todos />
+              </div>
             </>
           )}
           {status !== "loading" && !sessionData && (
-                        // status が "loading" でない、つまり認証情報の取得が完了している、
+            // status が "loading" でない、つまり認証情報の取得が完了している、
             // かつ、認証されていない場合に、下記が表示されます
             <div className="flex flex-col items-center">
               <button
@@ -54,36 +57,19 @@ function Home() {
                   - The ultimate productivity tool -
                 </p>
               </div>
+              <div className="">
+                <Image
+                  src="/images/main-img.png"
+                  width={600}
+                  height={600}
+                  alt="main-img"
+                />
+              </div>
             </div>
           )}
         </main>
       </div>
-
     </>
-  );
-}
-
-function AuthShowcase() {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
   );
 }
 
